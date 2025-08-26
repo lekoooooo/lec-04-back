a5fd24a39ce4e0ac2db089909bb45d50c673b936
Levan Didebashvili <levdidebashvili@gmail.com>
2025-08-26 22:41:17 +0400

    feat

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 // const PORT = process.env.PORT || 3000;
 // server.listen(3000, () => console.log(`server is running on port ${PORT}`));
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
@@ -32,10 +34,56 @@ const PORT = 3000;
 //   res.send("query string");
 // });
 
-app.get("/", (req, res) => {
-  res.json({ message: "welcome", author: "tata" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ message: "welcome", author: "tata" });
+// });
+///////////////////////////////////////////////////////////////////////////
+app.use(express.json());
 
+// app.use(express.static("public"));
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//third party
+// /assets/new text document.txt ანუ app.use(express.static("public"));
+// ^^ უყურებს public-ში მყოფ ელემენტებს ^^
+// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+// app.use(morgan("dev"));
+
+// app.use(cors());
+// app.use((req, res, next) => {
+//   console.log(`[custom] ${req.method}, ${req.originalUrl}`);
+//   next();
+// });
+
+// app.get(
+//   "/about",
+//   (req, res, next) => {
+//     console.log("hello from middleware");
+//     next();
+//   },
+//   (req, res) => {
+//     res.send("welcome");
+//   }
+// );
+
+const adminAuthMiddleware = (req, res, next) => {
+  const users = [
+    { id: 1, username: "zo", role: "user" },
+    { id: 2, username: "tata", role: "admin" },
+  ];
+  const user = users[1];
+  if (user.role !== "admin") {
+    return res.status(403).json({ message: "access denied ! admins only" });
+  }
+  next();
+};
+app.get("/admin", adminAuthMiddleware, (req, res) => {
+  res.send("ADMIN PAGE");
+});
+app.use((err, req, res, next) => {
+  res.json({
+    message: err,
+  });
+});
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
 });
